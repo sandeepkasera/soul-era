3b8f5480035ee67afacddfcf47e3158118c3e14b
Sandeep Kumar <sandeepkasera>
2024-06-02 19:14:36 +0530

    initial commit

---
+++ src/app/(pages)/page.tsx
@@ -0,0 +1,9 @@
+import Products from "./products/page";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Products/>
+    </main>
+  );
+}
---
+++ src/app/(pages)/products/page.tsx
@@ -0,0 +1,8 @@
+const products = () => {
+  return (
+    <div>
+      <h1>See all Products</h1>      
+    </div>
+  );
+};
+export default products;
