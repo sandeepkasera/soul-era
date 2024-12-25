b5d02d888d8f1e447db2b58a91172416eefa79db
Sandeep Kumar <sandeepkasera>
2024-12-25 19:46:28 +0530

    t-shirt pics

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,6 +4,9 @@ import Link from 'next/link';
 import { FaBars, FaTimes } from 'react-icons/fa'; // Using Font Awesome for hamburger menu icons
 import { CgProfile } from "react-icons/cg";
 import { useRouter } from "next/navigation";
+import { ShoppingCartIcon } from '@heroicons/react/24/outline';
+import clsx from 'clsx';
+
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -12,20 +15,23 @@ const Header = () => {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) {
-        const part = parts.pop();
-        if (part) {
-            return part.split(';').shift();
-        }
+      const part = parts.pop();
+      if (part) {
+        return part.split(';').shift();
+      }
     }
     return undefined;
-}
+  }
   const handleProfilePage = async () => {
     const user = getCookie('shit_user');
-        if (user) {
-            router.push(`/profile/${user}`);
-        } else {
-          router.push('/login');
-        }
+    if (user) {
+      router.push(`/profile/${user}`);
+    } else {
+      router.push('/login');
+    }
+  }
+  const handleCartPage = async () => {
+      router.push(`/cart`);
   }
 
   // const searchProducts = ({ products, searchQuery }) => {
@@ -52,9 +58,6 @@ const Header = () => {
             <Link href="/">Home</Link>
           </li>
           <li className="p-4 md:p-0 md:mx-2 hover:text-gray-300">
-            <Link href="/cart">Cart</Link>
-          </li>
-          <li className="p-4 md:p-0 md:mx-2 hover:text-gray-300">
             <form action="#" method="GET" className="flex items-center w-full">
               <input type="text" name="search" placeholder="Search Products" className="px-4 py-2 w-full md:w-auto border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 text-gray-800" />
               <button type="submit" onClick={searchProducts} className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
@@ -64,7 +67,14 @@ const Header = () => {
           </li>
           <li className="p-4 md:p-0 md:mx-2 hover:text-gray-300">
             <button onClick={handleProfilePage} className="text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-              <CgProfile />Profile
+              <CgProfile className={clsx('h-4 transition-all ease-in-out hover:scale-110')} />
+            </button>
+          </li>
+          <li className="p-4 md:p-0 md:mx-2 hover:text-gray-300">
+            <button onClick={handleCartPage} className="text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+              <ShoppingCartIcon
+                className={clsx('h-4 transition-all ease-in-out hover:scale-110')}
+              />
             </button>
           </li>
         </ul>
